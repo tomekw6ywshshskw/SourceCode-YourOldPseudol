46a3f5a949416d81dd6865636610a97b349df62d
vKexterPLv <120136795+vKexterPLv@users.noreply.github.com>
2024-04-22 13:01:48 +0200

    Update hud.lua

--- hud/hud.lua
+++ hud/hud.lua
@@ -1,63 +1,69 @@
---[[
-autor:TwojStary(mlody hill)
-w raie problemów pisz mlody.hill@interia.pl
---]]
-local components = { "weapon", "ammo", "health", "clock", "money", "breath", "armour", "wanted" }
-addEventHandler("onClientResourceStart", getResourceRootElement(getThisResource()),
-function ()
-	for _, component in ipairs( components ) do
-		setPlayerHudComponentVisible( component, false )
-	end
-end)
+local HUD = {}
 
 local screenWidth, screenHeight = guiGetScreenSize()
-local x, y = (screenWidth / 1980), (screenHeight / 1080) -- Współczynniki skalowania
+local x, y = (screenWidth / 1980), (screenHeight / 1080)
 
-local hudVisible = true
+function HUD:init()
+  self.show = true
+  self.func = {}
+  self.func.turnOff = function() self:hideComponents() end
+  self.func.render = function() self:draw() end
+  
+  addEventHandler("onClientRender",root,self.func.render)
+  addEventHandler("onClientResourceStart",resourceRoot,self.func.turnOff)
+end
 
-local function formatNumber(number)
-    local formattedNumber = tostring(number)
-    local formattedString = ""
-    local length = string.len(formattedNumber)
-    for i = 1, length do
-        if (i % 3 == 1) and (i ~= 1) then
-            formattedString = "," .. formattedString
-        end
-        formattedString = string.sub(formattedNumber, length - i + 1, length - i + 1) .. formattedString
+function HUD:formatNumber(number)
+  local formattedNumber = tostring(number)
+  local formattedString = ""
+  local length = string.len(formattedNumber)
+  for i = 1, length do
+    if (i % 3 == 1) and (i ~= 1) then
+      formattedString = "," .. formattedString
     end
-    return formattedString
+    formattedString = string.sub(formattedNumber, length - i + 1, length - i + 1) .. formattedString
+  end
+  return formattedString
 end
 
-function drawHUD()
-    local player = getLocalPlayer()
-    if not player then return end
-
-
-    local playerSrp = getElementData(player, "player:reputation") or 0
-    local playerMoney = getPlayerMoney(player)
-    local formattedMoney = formatNumber(playerMoney) -- Formatowanie ilości pieniędzy
-    local playerName = getPlayerName(player)
-    local plainPlayerName = playerName:gsub("#%x%x%x%x%x%x", "")
-    local maxHealth = 100 -- maksymalne zdrowie gracza
-    local playerHealth = getElementHealth(player)
-    local healthPercentage = playerHealth / maxHealth
-    local barWidth = 200 * x * healthPercentage -- szerokość paska HP zależna od procentu zdrowia
-
-  dxDrawRectangle(screenWidth - (200 * x), 0, barWidth, 15 * y, tocolor(255, 0, 0, 150)) -- Pasek HP czerwony
-  dxDrawText("Nick: " .. plainPlayerName .. " | SRP: " .. playerSrp, screenWidth - (190 * x), 20 * y, screenWidth, screenHeight, tocolor(255, 255, 255), 1, "default-bold", "left", "top")
-  dxDrawText("$ " .. formattedMoney, screenWidth - (190 * x), 40 * y, screenWidth, screenHeight, tocolor(255, 255, 255), 1, "default-bold", "left", "top")
+function HUD:setVariables()
+  local player = localPlayer
+  self.playerSrp = getElementData(player, "player:reputation") or 0
+  self.playerMoney = getPlayerMoney(player)
+  self.formattedMoney = formatNumber(playerMoney) -- Formatowanie ilości pieniędzy
+  self.playerName = getPlayerName(player)
+  self.plainlayerName = playerName:gsub("#%x%x%x%x%x%x", "")
+  self.maxHealth = 100
+  self.playerHealth = getElementHealth(player)
+  self.healthPercentage = playerHealth / maxHealth
+  self.barWidth = 200 * x * healthPercentage 
 end
 
-local function toggleHUD(key, state)
-    if key == "F7" and state == "down" then
-        hudVisible = not hudVisible
-    end
+function HUD:draw()
+  self:setVariables()
+  dxDrawRectangle(screenWidth - (200 * x), 0, self.barWidth, 15 * y, tocolor(255, 0, 0, 150)) -- Pasek HP czerwony
+  dxDrawText("Nick: " .. self.plainPlayerName .. " | SRP: " .. self.playerSrp, screenWidth - (190 * x), 20 * y, screenWidth, screenHeight, tocolor(255, 255, 255), 1, "default-bold", "left", "top")
+  dxDrawText("$ " .. self.formattedMoney, screenWidth - (190 * x), 40 * y, screenWidth, screenHeight, tocolor(255, 255, 255), 1, "default-bold", "left", "top")
 end
-bindKey("F7", "down", toggleHUD)
 
-local function renderHUD()
-    if hudVisible then
-        drawHUD()
-    end
+function HUD:hideComponents()
+  local components = { "weapon", "ammo", "health", "clock", "money", "breath", "armour", "wanted" }
+  for _,v in pairs(components) do
+    setPlayerHudComponentVisible( v, false )
+  end
 end
-addEventHandler("onClientRender", root, renderHUD)
+
+function HUD:toggleHUD(state)
+  if state then
+    addEventHandler("onClientRender",root,self.func.render)
+  else
+    removeEventHandler("onClientRender",root,self.func.render)
+  end
+end
+
+HUD:init()
+
+--DLA EXPORTU
+function showHUD(state)
+  HUD:toggleHUD(state)
+end
