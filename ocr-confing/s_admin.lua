e1f91ad36af23e4fcdf233424864b25924b17545
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-04-24 20:07:08 +0200

    Update s_admin.lua bug fixes, it should be fine now

--- ocr-confing/s_admin.lua
+++ ocr-confing/s_admin.lua
@@ -1,38 +1,31 @@
-
 local ranks = {
-    ["support"] = {
-        name = "Support",
-        color = {
-            hex = "#01bf34",
-            rgb = tocolor(1, 191, 52),
+    ["Support"] = {
+        colors = {
+            hex = "#ffffff",
+            rgb = tocolor(255, 255, 255),
         },
-        index = 1,
         permissions = {
             ["admins"] = true,
             ["duty"] = true,
             ["ucho"] = true,
         },
     },
-    ["moderator"] = {
-        name = "Moderator",
-        color = {
+    ["Moderator"] = {
+        colors = {
             hex = "#FF0000",
             rgb = tocolor(255, 0, 0),
         },
-        index = 2,
         permissions = {
             ["admins"] = true,
             ["duty"] = true,
             ["ucho"] = true,
         },
     },
-    ["administrator"] = {
-        name = "Administrator",
-        color = {
+    ["Administrator"] = {
+        colors = {
             hex = "#a31303",
             rgb = tocolor(163, 19, 3),
         },
-        index = 3,
         permissions = {
             ["admins"] = true,
             ["duty"] = true,
@@ -40,13 +33,11 @@ local ranks = {
             ["rapsy"] = true,
         },
     },
-    ["administrator_root"] = {
-        name = "Administrator root",
-        color = {
+    ["Administrator root"] = {
+        colors = {
             hex = "#FF007F",
             rgb = tocolor(255, 0, 127),
         },
-        index = 4,
         permissions = {
             ["admins"] = true,
             ["duty"] = true,
@@ -55,13 +46,11 @@ local ranks = {
             ["cl"] = true,
         },
     },
-    ["opiekun_administracji"] = {
-        name = "Opiekun administracji",
-        color = {
+    ["Opiekun administracji"] = {
+        colors = {
             hex = "#0000FF",
             rgb = tocolor(0, 0, 255),
         },
-        index = 5,
         permissions = {
             ["admins"] = true,
             ["duty"] = true,
@@ -70,13 +59,11 @@ local ranks = {
             ["cl"] = true,
         },
     },
-    ["zarzad"] = {
-        name = "Zarząd",
-        color = {
+    ["Zarząd"] = {
+        colors = {
             hex = "#FFFF00",
             rgb = tocolor(255, 255, 0),
         },
-        index = 6,
         permissions = {
             ["admins"] = true,
             ["duty"] = true,
@@ -87,71 +74,66 @@ local ranks = {
     },
 }
 
-function isPlayerInRank(player, rank)
-    local playerRank
-    if getElementData(player, "player:level") then
-        playerRank = getElementData(player, "player:level")
-    else
-        return false
-    end
-
-    if ranks[playerRank] and ranks[rank] then
-        local playerRankIndex = ranks[playerRank].index
-        local rankIndex = ranks[rank].index
-        return playerRankIndex >= rankIndex
-    else
-        return false
-    end
+function isPlayerInRank(playerName, rank)
+    local playerPermissions = ranks[playerName] and ranks[playerName].permissions
+    local rankPermissions = ranks[rank] and ranks[rank].permissions
+    return playerPermissions and rankPermissions and next(playerPermissions) == next(rankPermissions)
 end
 
 function cmd_admins(plr)
-    if not isPlayerInRank(plr, "support") then
-        outputChatBox("* You do not have permissions.", plr, 255, 0, 0)
+    local playerName = getPlayerName(plr)
+    if not isPlayerInRank(playerName, "Support") then
+        outputChatBox("* Nie masz wystarczających uprawnień.", plr, 255, 0, 0)
         return
     end
 
     -- Logika dla komendy /admins
 end
-addCommandHandler("admins", cmd_admins)
 
 function cmd_duty(plr)
-    if not isPlayerInRank(plr, "support") then
-        outputChatBox("* You do not have permissions.", plr, 255, 0, 0)
+    local playerName = getPlayerName(plr)
+    if not isPlayerInRank(playerName, "Support") then
+        outputChatBox("* Nie masz wystarczających uprawnień.", plr, 255, 0, 0)
         return
     end
 
     -- Logika dla komendy /duty
 end
-addCommandHandler("duty", cmd_duty)
 
 function cmd_ucho(plr)
-    if not isPlayerInRank(plr, "moderator") then
-        outputChatBox("* You do not have permissions.", plr, 255, 0, 0)
+    local playerName = getPlayerName(plr)
+    if not ranks[playerName] or not ranks[playerName].permissions.ucho then
+        outputChatBox("* Nie masz wystarczających uprawnień.", plr, 255, 0, 0)
         return
     end
 
     local x = getElementData(plr, "player:ucho")
     setElementData(plr, "player:ucho", not x)
 end
-addCommandHandler("ucho", cmd_ucho)
 
 function cmd_rapsy(plr, cmd, uid)
-    if not isPlayerInRank(plr, "administrator") and not isPlayerInRank(plr, "administrator_root") and not isPlayerInRank(plr, "opiekun_administracji") and not isPlayerInRank(plr, "zarzad") then
-        outputChatBox("* You do not have permissions.", plr, 255, 0, 0)
+    local playerName = getPlayerName(plr)
+    if not ranks[playerName] or not ranks[playerName].permissions.rapsy then
+        outputChatBox("* Nie masz wystarczających uprawnień.", plr, 255, 0, 0)
         return
     end
 
     -- Logika dla komendy /rapsy
 end
-addCommandHandler("rapsy", cmd_rapsy)
 
 function cmd_cl(plr, cmd, id, ...)
-    if not isPlayerInRank(plr, "administrator_root") and not isPlayerInRank(plr, "zarzad") then
-        outputChatBox("* You do not have permissions.", plr, 255, 0, 0)
+    local playerName = getPlayerName(plr)
+    if not ranks[playerName] or not ranks[playerName].permissions.cl then
+        outputChatBox("* Nie masz wystarczających uprawnień.", plr, 255, 0, 0)
         return
     end
 
     -- Logika dla komendy /cl
 end
-addCommandHandler("cl", cmd_cl)
 
+-- Dodajemy obsługę komend:
+addCommandHandler("admins", cmd_admins)
+addCommandHandler("duty", cmd_duty)
+addCommandHandler("ucho", cmd_ucho)
+addCommandHandler("rapsy", cmd_rapsy)
+addCommandHandler("cl", cmd_cl)
